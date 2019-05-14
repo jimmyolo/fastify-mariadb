7154d8b5bd1149195ab62ab31da29cd5b347f45e
Jimmy Huang <victor0801x@gmail.com>
2019-05-14 16:06:56 +0800

    switch to standard lint (#9)
    * switch to standard lint
    * remove eslint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-const fp = require('fastify-plugin');
-const { format, escape, escapeId } = require('sqlstring');
+const fp = require('fastify-plugin')
+const { format, escape, escapeId } = require('sqlstring')
 
-function activatePool(options, cb) {
-  const usePromise = options.promise;
-  delete options.promise;
+function activatePool (options, cb) {
+  const usePromise = options.promise
+  delete options.promise
 
-  const mysql = usePromise ? require('mariadb/promise') : require('mariadb/callback');
-  const pool = mysql.createPool(options.connectionString || options);
+  const mysql = usePromise ? require('mariadb/promise') : require('mariadb/callback')
+  const pool = mysql.createPool(options.connectionString || options)
   const fastifyPool = {
     pool,
     query: pool.query.bind(pool),
@@ -16,56 +16,56 @@ function activatePool(options, cb) {
     sqlstring: {
       format,
       escape,
-      escapeId,
-    },
-  };
+      escapeId
+    }
+  }
   if (usePromise) {
     pool.query('select 1')
       .then(() => cb(null, fastifyPool))
-      .catch((err) => cb(err, null));
+      .catch((err) => cb(err, null))
   } else {
-    pool.query('select 1', (err) => cb(err, fastifyPool));
+    pool.query('select 1', (err) => cb(err, fastifyPool))
   }
 }
 
-function fastifyMariadb(fastify, options, next) {
-  const name = options.name;
-  delete options.name;
+function fastifyMariadb (fastify, options, next) {
+  const name = options.name
+  delete options.name
 
-  const usePromise = options.promise;
+  const usePromise = options.promise
 
   activatePool(options, (err, fastifyPool) => {
-    if (err) return next(err);
+    if (err) return next(err)
 
-    const pool = fastifyPool.pool;
+    const pool = fastifyPool.pool
 
     if (usePromise) {
-      fastify.addHook('onClose', () => pool.end());
+      fastify.addHook('onClose', () => pool.end())
     } else {
-      fastify.addHook('onClose', (fastify, done) => pool.end(done));
+      fastify.addHook('onClose', (fastify, done) => pool.end(done))
     }
 
     if (name) {
       if (!fastify.mariadb) {
-        fastify.decorate('mariadb', {});
+        fastify.decorate('mariadb', {})
       }
       if (fastify.mariadb[name]) {
-        return next(new Error('fastify.mariadb.' + name + ' has already registered'));
+        return next(new Error('fastify.mariadb.' + name + ' has already registered'))
       }
-      fastify.mariadb[name] = fastifyPool;
+      fastify.mariadb[name] = fastifyPool
     } else {
       if (fastify.mariadb) {
-        return next(new Error('fastify.mariadb has already registered'));
+        return next(new Error('fastify.mariadb has already registered'))
       } else {
-        fastify.mariadb = fastifyPool;
+        fastify.mariadb = fastifyPool
       }
     }
 
-    next();
-  });
+    next()
+  })
 }
 
 module.exports = fp(fastifyMariadb, {
   fastify: '>=1.x',
-  name: 'fastify-mariadb',
-});
+  name: 'fastify-mariadb'
+})
--- test/pool.callback.test.js
+++ test/pool.callback.test.js
@@ -1,161 +1,160 @@
-'use strict';
-const TEST_HOST = process.env.TEST_HOST || 'localhost';
-const TEST_PORT = process.env.TEST_PORT || 3306;
-const TEST_USER = process.env.TEST_USER || 'root';
-const TEST_DB = process.env.TEST_DB || 'mysql';
+'use strict'
+const TEST_HOST = process.env.TEST_HOST || 'localhost'
+const TEST_PORT = process.env.TEST_PORT || 3306
+const TEST_USER = process.env.TEST_USER || 'root'
+const TEST_DB = process.env.TEST_DB || 'mysql'
 
-const test = require('tap').test;
-const Fastify = require('fastify');
-const fastifyMariadb = require('../index');
+const test = require('tap').test
+const Fastify = require('fastify')
+const fastifyMariadb = require('../index')
 
 test('fastify.mariadb plugin', (batch) => {
-  let fastify = false;
+  let fastify = false
   batch.beforeEach((done) => {
-    fastify = Fastify();
+    fastify = Fastify()
     fastify.register(fastifyMariadb, {
       host: TEST_HOST,
       user: TEST_USER,
       database: 'mysql',
       connectionLimit: 5,
-      // Compatibility option. causes Promise to return an array object, [rows, metadata].
-      // rather than the rows as JSON objects with a meta property.
-      metaAsArray: true,
-    });
-    done();
-  });
+
+      metaAsArray: true
+    })
+    done()
+  })
 
   batch.afterEach((done) => {
-    fastify.close();
-    fastify = null;
-    done();
-  });
+    fastify.close()
+    fastify = null
+    done()
+  })
 
   batch.test('fastify.mariadb namespace should exist', (t) => {
-    t.plan(6);
+    t.plan(6)
     fastify.ready((err) => {
-      t.error(err);
-      t.ok(fastify.mariadb);
-      t.ok(fastify.mariadb.pool);
-      t.ok(fastify.mariadb.query);
-      t.ok(fastify.mariadb.getConnection);
-      t.ok(fastify.mariadb.sqlstring);
-    });
-  });
+      t.error(err)
+      t.ok(fastify.mariadb)
+      t.ok(fastify.mariadb.pool)
+      t.ok(fastify.mariadb.query)
+      t.ok(fastify.mariadb.getConnection)
+      t.ok(fastify.mariadb.sqlstring)
+    })
+  })
 
   batch.test('mariadb.pool.query', (t) => {
-    t.plan(4);
+    t.plan(4)
     fastify.ready((err) => {
-      t.error(err);
+      t.error(err)
       fastify.mariadb.query('SELECT 1 AS `ping`', (err, [results, metadata]) => {
-        t.error(err);
-        t.ok(results[0].ping === 1);
-        t.ok(metadata);
-      });
-    });
-  });
+        t.error(err)
+        t.ok(results[0].ping === 1)
+        t.ok(metadata)
+      })
+    })
+  })
 
   batch.test('pool.getConnection', (t) => {
-    t.plan(6);
+    t.plan(6)
     fastify.ready((err) => {
-      t.error(err);
+      t.error(err)
       fastify.mariadb.getConnection((err, connection) => {
-        t.error(err);
+        t.error(err)
         connection.query('SELECT 2 AS `ping`', (err, [results]) => {
-          t.error(err);
-          t.ok(results[0].ping === 2);
-          connection.release();
-        });
-      });
+          t.error(err)
+          t.ok(results[0].ping === 2)
+          connection.release()
+        })
+      })
       fastify.mariadb.query('SELECT 3 AS `ping`', (err, [results]) => {
-        t.error(err);
-        t.ok(results[0].ping === 3);
-      });
-    });
-  });
+        t.error(err)
+        t.ok(results[0].ping === 3)
+      })
+    })
+  })
 
   batch.test('synchronous sqlstring utils', (t) => {
-    t.plan(4);
+    t.plan(4)
     fastify.ready((err) => {
-      t.error(err);
-      const sqlstring = fastify.mariadb.sqlstring;
+      t.error(err)
+      const sqlstring = fastify.mariadb.sqlstring
 
       t.is(
         sqlstring.format('SELECT ? AS `now`', [1]),
         'SELECT 1 AS `now`'
-      );
+      )
 
-      const id = 'userId';
+      const id = 'userId'
       t.is(
         'SELECT * FROM users WHERE id = ' + sqlstring.escape(id),
         `SELECT * FROM users WHERE id = '${id}'`
-      );
+      )
 
-      const sorter = 'date';
+      const sorter = 'date'
       t.is(
         'SELECT * FROM posts ORDER BY ' + sqlstring.escapeId('posts.' + sorter),
         'SELECT * FROM posts ORDER BY `posts`.`date`'
-      );
-    });
-  });
+      )
+    })
+  })
 
-  batch.end();
-});
+  batch.end()
+})
 
 test('fastify.mariadb.test namespace should exist', (t) => {
-  t.plan(6);
+  t.plan(6)
 
-  const fastify = Fastify();
+  const fastify = Fastify()
   fastify
     .register(fastifyMariadb, {
       name: 'test',
-      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`,
-    });
+      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`
+    })
 
   fastify.ready((err) => {
-    t.error(err);
-    t.ok(fastify.mariadb);
-    t.ok(fastify.mariadb.test);
-    t.ok(fastify.mariadb.test.pool);
-    t.ok(fastify.mariadb.test.getConnection);
-    t.ok(fastify.mariadb.test.sqlstring);
-    fastify.close();
-  });
-});
+    t.error(err)
+    t.ok(fastify.mariadb)
+    t.ok(fastify.mariadb.test)
+    t.ok(fastify.mariadb.test.pool)
+    t.ok(fastify.mariadb.test.getConnection)
+    t.ok(fastify.mariadb.test.sqlstring)
+    fastify.close()
+  })
+})
 
 test('fastify.mariadb should throw has already registered', (t) => {
-  t.plan(1);
+  t.plan(1)
 
-  const fastify = Fastify();
+  const fastify = Fastify()
   fastify
     .register(fastifyMariadb, {
-      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`,
+      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`
     })
     .register(fastifyMariadb, {
-      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`,
-    });
+      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`
+    })
 
   fastify.ready((err) => {
-    t.is(err.message, 'fastify.mariadb has already registered');
-    fastify.close();
-  });
-});
+    t.is(err.message, 'fastify.mariadb has already registered')
+    fastify.close()
+  })
+})
 
 test('fastify.mariadb.test should throw has already registered', (t) => {
-  t.plan(1);
+  t.plan(1)
 
-  const fastify = Fastify();
+  const fastify = Fastify()
   fastify
     .register(fastifyMariadb, {
       name: 'test',
-      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`,
+      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`
     })
     .register(fastifyMariadb, {
       name: 'test',
-      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`,
-    });
+      connectionString: `mariadb://${TEST_USER}@${TEST_HOST}:${TEST_PORT}/${TEST_DB}`
+    })
 
   fastify.ready((err) => {
-    t.is(err.message, 'fastify.mariadb.test has already registered');
-    fastify.close();
-  });
-});
+    t.is(err.message, 'fastify.mariadb.test has already registered')
+    fastify.close()
+  })
+})
